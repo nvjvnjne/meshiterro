e37df50fce1b4cd272cb75981515b740a5db1fd4
nvjvnjne <113250918+nvjvnjne@users.noreply.github.com>
2022-11-17 03:08:33 +0000

    [finish]4-20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @postimages = @user.post_images
+    @post_images = @user.post_images
   end
 
   def update
